c1d2d0d5d2198106b600dd90d78e168c35cff04f
Stepan Startsev <startsev.sv@perfema.com>
2020-10-28 21:16:27 +0300

    init

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,27 @@
+import cats.effect._
+import fs2.Stream
+import org.http4s._
+import org.http4s.dsl.Http4sDsl
+import org.http4s.syntax.kleisli._
+import org.http4s.server.blaze.BlazeServerBuilder
+import scala.concurrent.ExecutionContext.global
+
+object Main extends IOApp {
+  def run(args: List[String]): IO[ExitCode] =
+    ExampleApp.serverStream[IO].compile.drain.as(ExitCode.Success)
+}
+
+object ExampleApp {
+  def serverStream[F[_]: ConcurrentEffect: Timer]: Stream[F, ExitCode] =
+    BlazeServerBuilder[F](global)
+      .bindHttp(7575, "0.0.0.0")
+      .withHttpApp(ExampleRoutes[F]().routes.orNotFound)
+      .serve
+}
+
+case class ExampleRoutes[F[_]: Sync]() extends Http4sDsl[F] {
+  val routes: HttpRoutes[F] =
+    HttpRoutes.of[F] { case GET -> Root / "ping" / name=>
+      Ok(s"ping, $name.")
+    }
+}
